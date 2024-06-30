71458f796005be4c8c4c7670e13796fa81db1dec
damiennarozniak <damiennarozniak@yahoo.com>
2024-06-30 04:37:19 +0000

    deleted connectbutton on the left

--- genartory/frontend/src/components/layout/Header.tsx
+++ genartory/frontend/src/components/layout/Header.tsx
@@ -10,20 +10,11 @@ import { WalletConnector } from "@aptos-labs/wallet-adapter-mui-design";
 const Header = () => {
   const { connect, account } = useWallet();
 
-  const connectWallet = async () => {
-    try {
-      await connect();
-      console.log('Connected account:', account);
-      // You can add more logic here to set the account in your app's state
-    } catch (error) {
-      console.error('Error connecting to wallet:', error);
-    }
-  };
 
   return (
     <header className={styles.header}>
       <h1>My App</h1>
-          <button className={styles.connectWalletButton} onClick={connectWallet}>Connect Wallet</button>
+
           <WalletConnector />
     </header>
   );
