d9e1998af7ae7389a6b7e7c4788f2cd60d757aa2
damiennarozniak <damiennarozniak@yahoo.com>
2024-07-01 03:43:10 +0000

    some style changes

--- genartory/frontend/src/components/layout/Header.tsx
+++ genartory/frontend/src/components/layout/Header.tsx
@@ -1,5 +1,4 @@
 // Updated Header.tsx with a "Back to Home" button
-
 import React from 'react';
 import styles from './Header.module.css'; // Corrected import to use Header.module.css
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
@@ -7,24 +6,25 @@ import { WalletConnector } from "@aptos-labs/wallet-adapter-mui-design";
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom for navigation
 
 const Header = () => {
-  const { connect, account } = useWallet();
+  const { connect, account, connected } = useWallet();
   return (
     <header className={styles.header}>
       <h1>My App</h1>
-      <Link to="/" className={styles.homeButton}>Home</Link> {/* Add a "Back to Home" button */}
-      <div className={styles.headerRight}>
-      <Link to="/create" className={styles.homeButton}>Create</Link>
-      <Link to="/explore" className={styles.homeButton}>Explore</Link>
-      <Link to="/my-collections" className={styles.homeButton}>My Collections</Link>
-      <Link to="/activity" className={styles.homeButton}>Activity</Link>
-      <Link to="/my-nfts" className={styles.homeButton}>My NFTs</Link>
-      <Link to="/profile" className={styles.homeButton}>Profile</Link>
-
-
-      </div>
-<WalletConnector />
       
+      <Link to="/" className={styles.homeButton}>Home</Link> {/* Add a "Back to Home" button */}
 
+      {connected ? 
+        <div className={styles.headerRight}>
+          <Link to="/create" className={styles.homeButton}>Create</Link>
+          <Link to="/explore" className={styles.homeButton}>Explore</Link>
+          <Link to="/my-collections" className={styles.homeButton}>My Collections</Link>
+          <Link to="/activity" className={styles.homeButton}>Activity</Link>
+          <Link to="/my-nfts" className={styles.homeButton}>My NFTs</Link>
+          <Link to="/profile" className={styles.homeButton}>Profile</Link>
+        </div>
+       : <div className={styles.welcomeMessage}>WELCOME</div>}
+      
+      <WalletConnector />
     </header>
   );
 };
